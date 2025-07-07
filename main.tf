90d9743efb48c17d966905a6ef940c3e933522f5
Philip4114 <manassas-gold@hotmail.com>
2025-07-07 08:21:38 -0400

    corrected listeners block

--- main.tf
+++ main.tf
@@ -51,13 +51,14 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
   security_groups = module.blog_sg.security_group_id
 
-  listeners = {
+  
     http_tcp_listeners = [
-      port     = 80
-      protocol = "HTTP"
-      target_group_index = 0
-    ]
-  }
+      {
+        port     = 80
+        protocol = "HTTP"
+        target_group_index = 0
+      }
+  ]
 
   target_groups = {
     ex-instance = {
