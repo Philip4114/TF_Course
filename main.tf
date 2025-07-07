c7f79319e1aa0b7bfaeed9b64beda9fa8f9954b6
Philip4114 <manassas-gold@hotmail.com>
2025-07-07 11:48:34 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -83,9 +83,9 @@ module "blog_alb" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
-  name = "blog"
   
   vpc_id = module.blog_vpc.vpc_id
+  name = "blog"
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
