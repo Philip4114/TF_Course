f46ecbb25eeee633506a01c010ea8a29a25c9e69
Philip4114 <manassas-gold@hotmail.com>
2025-07-07 14:40:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ module "blog_autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  autoscaling_group_target_group_arns   = module.blog_alb.target_group[0].arns
+  target_group_arns   = module.blog_alb.target_group[0]_arns
   security_groups                       = [module.blog_sg.security_group_id]
   
   instance_type = var.instance_type
