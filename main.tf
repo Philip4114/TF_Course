f79f06bb83c80544724c3ce549f2d43c6a345ef5
Philip4114 <manassas-gold@hotmail.com>
2025-07-07 08:42:44 -0400

    updated SG to include brackets

--- main.tf
+++ main.tf
@@ -43,13 +43,17 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
   name    = "blog-alb"
+
+  load_balancer_type = "application"
+
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  security_groups = [module.blog_sg.security_group_id]
   
   target_groups = [
     {
@@ -57,14 +61,8 @@ module "alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id        = aws_instance.blog.id
-          port = 80
-      }
     }
-  }
-]
+  ]
 
   http_tcp_listeners = [
     {
