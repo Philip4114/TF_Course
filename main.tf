9142a5f84768a0eb2e150217265f24de8b3b0eab
Philip4114 <manassas-gold@hotmail.com>
2025-07-08 07:13:59 -0400

    Add Load Balancer

--- main.tf
+++ main.tf
@@ -34,13 +34,61 @@ module "blog_vpc" {
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   subnet_id              = module.blog_vpc.public_subnets[0]
-
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "Learning Terraform"
+  }
+}
+
+module "blog-alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+
+  name    = "my-alb"
+
+
+
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+  security_groups = [mdule.blog_sg.security_group_id]
+
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+#    ex-https = {
+#      port            = 443
+#      protocol        = "HTTPS"
+#      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+#
+#      forward = {
+#        target_group_key = "ex-instance"
+#      }
+#    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.blog.id
+    }
+  }
+
+  tags = {
+    Environment = "Development"
+#    Project     = "Example"
   }
 }
 
